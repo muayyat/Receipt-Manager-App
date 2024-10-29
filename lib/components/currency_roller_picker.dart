5502ade86c4bba19bac737b954a2f36e2c7edc2e
Jingjing Yang <jingjing.yang@tuni.fi>
2024-10-29 18:46:04 +0200

    Use Default List of Currencies before success loading

--- lib/components/currency_roller_picker.dart
+++ lib/components/currency_roller_picker.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../logger.dart';
 import '../services/currency_service.dart';
 
-class CurrencyPicker extends StatefulWidget {
+class CurrencyPicker extends StatelessWidget {
   final String selectedCurrency;
   final ValueChanged<String> onCurrencySelected;
 
@@ -14,83 +14,243 @@ class CurrencyPicker extends StatefulWidget {
     required this.onCurrencySelected,
   });
 
-  @override
-  CurrencyPickerState createState() => CurrencyPickerState();
-}
-
-class CurrencyPickerState extends State<CurrencyPicker> {
-  String? tempSelectedCurrency;
-  List<String> availableCurrencies = []; // Initialize as empty list
-
-  @override
-  void initState() {
-    super.initState();
-    tempSelectedCurrency =
-        widget.selectedCurrency; // Initialize temporary value
-    fetchCurrencyCodes(); // Fetch currencies when widget is initialized
-  }
-
-  Future<void> fetchCurrencyCodes() async {
+  Future<List<String>> fetchCurrencyCodes() async {
     try {
-      availableCurrencies = await CurrencyService.fetchCurrencyCodes();
-      setState(() {}); // Update UI after fetching currency codes
+      return await CurrencyService.fetchCurrencyCodes();
     } catch (e) {
       logger.e('Failed to fetch available currencies: $e');
+      return []; // Return an empty list on error
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    if (availableCurrencies.isEmpty) {
-      return Center(
-          child: CircularProgressIndicator()); // Show loading indicator
-    }
+    return FutureBuilder<List<String>>(
+      future: fetchCurrencyCodes(),
+      builder: (context, snapshot) {
+        final List<String> defaultCodelist = [
+          'AED',
+          'AFN',
+          'ALL',
+          'AMD',
+          'ANG',
+          'AOA',
+          'ARS',
+          'AUD',
+          'AWG',
+          'AZN',
+          'BAM',
+          'BBD',
+          'BDT',
+          'BGN',
+          'BHD',
+          'BIF',
+          'BMD',
+          'BND',
+          'BOB',
+          'BRL',
+          'BSD',
+          'BTC',
+          'BTN',
+          'BWP',
+          'BYN',
+          'BZD',
+          'CAD',
+          'CDF',
+          'CHF',
+          'CLF',
+          'CLP',
+          'CNH',
+          'CNY',
+          'COP',
+          'CRC',
+          'CUC',
+          'CUP',
+          'CVE',
+          'CZK',
+          'DJF',
+          'DKK',
+          'DOP',
+          'DZD',
+          'EGP',
+          'ERN',
+          'ETB',
+          'EUR',
+          'FJD',
+          'FKP',
+          'GBP',
+          'GEL',
+          'GGP',
+          'GHS',
+          'GIP',
+          'GMD',
+          'GNF',
+          'GTQ',
+          'GYD',
+          'HKD',
+          'HNL',
+          'HRK',
+          'HTG',
+          'HUF',
+          'IDR',
+          'ILS',
+          'IMP',
+          'INR',
+          'IQD',
+          'IRR',
+          'ISK',
+          'JEP',
+          'JMD',
+          'JOD',
+          'JPY',
+          'KES',
+          'KGS',
+          'KHR',
+          'KMF',
+          'KPW',
+          'KRW',
+          'KWD',
+          'KYD',
+          'KZT',
+          'LAK',
+          'LBP',
+          'LKR',
+          'LRD',
+          'LSL',
+          'LYD',
+          'MAD',
+          'MDL',
+          'MGA',
+          'MKD',
+          'MMK',
+          'MNT',
+          'MOP',
+          'MRU',
+          'MUR',
+          'MVR',
+          'MWK',
+          'MXN',
+          'MYR',
+          'MZN',
+          'NAD',
+          'NGN',
+          'NIO',
+          'NOK',
+          'NPR',
+          'NZD',
+          'OMR',
+          'PAB',
+          'PEN',
+          'PGK',
+          'PHP',
+          'PKR',
+          'PLN',
+          'PYG',
+          'QAR',
+          'RON',
+          'RSD',
+          'RUB',
+          'RWF',
+          'SAR',
+          'SBD',
+          'SCR',
+          'SDG',
+          'SEK',
+          'SGD',
+          'SHP',
+          'SLL',
+          'SOS',
+          'SRD',
+          'SSP',
+          'STD',
+          'STN',
+          'SVC',
+          'SYP',
+          'SZL',
+          'THB',
+          'TJS',
+          'TMT',
+          'TND',
+          'TOP',
+          'TRY',
+          'TTD',
+          'TWD',
+          'TZS',
+          'UAH',
+          'UGX',
+          'USD',
+          'UYU',
+          'UZS',
+          'VEF',
+          'VES',
+          'VND',
+          'VUV',
+          'WST',
+          'XAF',
+          'XAG',
+          'XAU',
+          'XCD',
+          'XDR',
+          'XOF',
+          'XPD',
+          'XPF',
+          'XPT',
+          'YER',
+          'ZAR',
+          'ZMW',
+          'ZWL'
+        ];
 
-    int initialIndex = availableCurrencies.indexOf(widget.selectedCurrency);
+        // Use a default list until data loads
+        final List<String> currencyList =
+            snapshot.hasData && snapshot.data!.isNotEmpty
+                ? snapshot.data!
+                : defaultCodelist; // Default currencies
 
-    return Container(
-      padding: EdgeInsets.all(16),
-      height: 300, // Set an appropriate height for the picker
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Select Currency',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Expanded(
-                  child: CupertinoPicker(
-                    scrollController: FixedExtentScrollController(
-                        initialItem: initialIndex == -1 ? 0 : initialIndex),
-                    itemExtent: 32.0, // Height of each item
-                    onSelectedItemChanged: (int index) {
-                      setState(() {
-                        tempSelectedCurrency = availableCurrencies[index];
-                      });
-                    },
-                    children: availableCurrencies
-                        .map((currency) => Center(child: Text(currency)))
-                        .toList(),
-                  ),
+        int initialIndex = currencyList.indexOf(selectedCurrency);
+        if (initialIndex == -1) initialIndex = 0;
+
+        return Container(
+          padding: EdgeInsets.all(16),
+          height: 300,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Select Currency',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: CupertinoPicker(
+                        scrollController: FixedExtentScrollController(
+                          initialItem: initialIndex == -1 ? 0 : initialIndex,
+                        ),
+                        itemExtent: 32.0,
+                        onSelectedItemChanged: (int index) {
+                          onCurrencySelected(currencyList[index]);
+                        },
+                        children: currencyList
+                            .map((currency) => Center(child: Text(currency)))
+                            .toList(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context); // Close the picker
+                      },
+                      child: Text('DONE'),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20),
-                TextButton(
-                  onPressed: () {
-                    if (tempSelectedCurrency != null) {
-                      widget.onCurrencySelected(tempSelectedCurrency!);
-                    }
-                    Navigator.pop(context); // Close the picker
-                  },
-                  child: Text('DONE'),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
