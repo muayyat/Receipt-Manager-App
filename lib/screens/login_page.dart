26dc2f1902c6f9f5cd399c68331e12202f3179ab
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-11 11:45:13 +0200

    Improve extractErrorMessage on login

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -26,12 +26,18 @@ class LogInPageState extends State<LogInPage> {
   // Method to extract error message from FirebaseAuthException
   String extractErrorMessage(FirebaseAuthException e) {
     switch (e.code) {
+      case 'invalid-email':
+        return 'The email address is not valid.';
+      case 'user-disabled':
+        return 'This user account has been disabled.';
       case 'user-not-found':
-        return 'No user found with this email.';
+        return 'No user found with this email. Please sign up first.';
       case 'wrong-password':
         return 'Incorrect password. Please try again.';
+      case 'invalid-credential':
+        return 'Invalid credentials. Please check your details.';
       default:
-        return 'An unexpected error occurred. Please try again later.';
+        return 'An unexpected error occurred. Please try again.';
     }
   }
 
@@ -56,173 +62,175 @@ class LogInPageState extends State<LogInPage> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            TextFormField(
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(color: textSecondaryColor),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: mainPurpleColor),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              onChanged: (value) {
-                password = value;
-              },
-              obscureText: !_isPasswordVisible, // Toggle password visibility
-              decoration: InputDecoration(
-                labelText: "Password",
-                labelStyle: TextStyle(color: textSecondaryColor),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: mainPurpleColor),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                    color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              TextFormField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: textSecondaryColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
-                    });
-                  },
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: mainPurpleColor),
+                  ),
                 ),
               ),
-            ),
-            if (errorMessage.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  errorMessage,
-                  style: TextStyle(color: Colors.red, fontSize: 14),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  errorMessage = ''; // Clear the error message
-                });
-
-                try {
-                  final user =
-                      await AuthService.signInWithEmail(email, password);
-
-                  if (user != null) {
-                    // Check if the email is verified
-                    if (user.emailVerified) {
-                      // Use a post-frame callback to handle navigation outside of async context
-                      WidgetsBinding.instance.addPostFrameCallback((_) {
-                        if (mounted) {
-                          Navigator.pushNamed(context, ReceiptListScreen.id);
-                        }
+              SizedBox(height: 16),
+              TextFormField(
+                onChanged: (value) {
+                  password = value;
+                },
+                obscureText: !_isPasswordVisible, // Toggle password visibility
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: textSecondaryColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: mainPurpleColor),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
                       });
-                    } else {
-                      // Sign out if email is not verified
-                      await AuthService.signOut();
-                      if (mounted) {
-                        setState(() {
-                          errorMessage =
-                              'Please verify your email before logging in.';
+                    },
+                  ),
+                ),
+              ),
+              if (errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    errorMessage,
+                    style: TextStyle(color: Colors.red, fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () async {
+                  setState(() {
+                    errorMessage = ''; // Clear the error message
+                  });
+
+                  try {
+                    final user =
+                        await AuthService.signInWithEmail(email, password);
+
+                    if (user != null) {
+                      // Check if the email is verified
+                      if (user.emailVerified) {
+                        // Use a post-frame callback to handle navigation outside of async context
+                        WidgetsBinding.instance.addPostFrameCallback((_) {
+                          if (mounted) {
+                            Navigator.pushNamed(context, ReceiptListScreen.id);
+                          }
                         });
+                      } else {
+                        // Sign out if email is not verified
+                        await AuthService.signOut();
+                        if (mounted) {
+                          setState(() {
+                            errorMessage =
+                                'Please verify your email before logging in.';
+                          });
+                        }
                       }
                     }
+                  } on FirebaseAuthException catch (e) {
+                    if (mounted) {
+                      setState(() {
+                        errorMessage = extractErrorMessage(e);
+                      });
+                    }
+                  } catch (e) {
+                    if (mounted) {
+                      setState(() {
+                        errorMessage =
+                            'An error occurred. Please try again later.';
+                      });
+                    }
                   }
-                } on FirebaseAuthException catch (e) {
-                  if (mounted) {
-                    setState(() {
-                      errorMessage = extractErrorMessage(e);
-                    });
-                  }
-                } catch (e) {
-                  if (mounted) {
-                    setState(() {
-                      errorMessage =
-                          'An error occurred. Please try again later.';
-                    });
-                  }
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: mainPurpleColor,
-                minimumSize: Size(double.infinity, 48),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              child: Text(
-                "Login",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  // Navigate to Forgot Password screen
-                  Navigator.pushNamed(context, ForgotPasswordPage.id);
                 },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: mainPurpleColor,
+                  minimumSize: Size(double.infinity, 48),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
                 child: Text(
-                  "Forgot Password?",
+                  "Login",
                   style: TextStyle(
-                    color: mainPurpleColor,
+                    color: Colors.white,
                     fontSize: 16,
-                    decoration: TextDecoration.underline,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 8),
-            Center(
-              child: Text.rich(
-                TextSpan(
-                  text: "Don’t have an account yet? ",
-                  style: TextStyle(color: textSecondaryColor, fontSize: 16),
-                  children: [
-                    TextSpan(
-                      text: "Sign Up",
-                      style: TextStyle(
-                        color: mainPurpleColor,
-                        fontSize: 16,
-                        decoration: TextDecoration.underline,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () {
-                          Navigator.pushNamed(context, SignUpPage.id);
-                        },
+              SizedBox(height: 16),
+              Center(
+                child: TextButton(
+                  onPressed: () {
+                    // Navigate to Forgot Password screen
+                    Navigator.pushNamed(context, ForgotPasswordPage.id);
+                  },
+                  child: Text(
+                    "Forgot Password?",
+                    style: TextStyle(
+                      color: mainPurpleColor,
+                      fontSize: 16,
+                      decoration: TextDecoration.underline,
                     ),
-                  ],
+                  ),
                 ),
               ),
-            ),
-          ],
+              SizedBox(height: 8),
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    text: "Don’t have an account yet? ",
+                    style: TextStyle(color: textSecondaryColor, fontSize: 16),
+                    children: [
+                      TextSpan(
+                        text: "Sign Up",
+                        style: TextStyle(
+                          color: mainPurpleColor,
+                          fontSize: 16,
+                          decoration: TextDecoration.underline,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            Navigator.pushNamed(context, SignUpPage.id);
+                          },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
