e0c8a70c2440280ac8bf7625927931efe8ece9f4
Jingjing Yang <jingjing.yang@tuni.fi>
2024-11-11 19:32:11 +0200

    Fix overflow issue

--- lib/screens/signup_page.dart
+++ lib/screens/signup_page.dart
@@ -117,164 +117,166 @@ class SignUpPageState extends State<SignUpPage> {
         ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            TextFormField(
-              controller: _userNameController,
-              onChanged: (value) {
-                userName = value;
-              },
-              decoration: InputDecoration(
-                labelText: "Name",
-                labelStyle: TextStyle(color: textSecondaryColor),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: mainPurpleColor),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              onChanged: (value) {
-                email = value;
-              },
-              decoration: InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(color: textSecondaryColor),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: mainPurpleColor),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            TextFormField(
-              onChanged: (value) {
-                password = value;
-              },
-              obscureText: !_isPasswordVisible, // Toggle password visibility
-              decoration: InputDecoration(
-                labelText: "Password",
-                labelStyle: TextStyle(color: textSecondaryColor),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: Colors.grey.shade300),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.0),
-                  borderSide: BorderSide(color: mainPurpleColor),
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
-                    color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              TextFormField(
+                controller: _userNameController,
+                onChanged: (value) {
+                  userName = value;
+                },
+                decoration: InputDecoration(
+                  labelText: "Name",
+                  labelStyle: TextStyle(color: textSecondaryColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
-                    });
-                  },
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: mainPurpleColor),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                Checkbox(
-                  value: _isChecked,
-                  onChanged: (value) {
-                    setState(() {
-                      _isChecked = value ?? false;
-                    });
-                  },
-                  activeColor: mainPurpleColor,
+              SizedBox(height: 16),
+              TextFormField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  labelStyle: TextStyle(color: textSecondaryColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: mainPurpleColor),
+                  ),
                 ),
-                Expanded(
-                  child: RichText(
-                    text: TextSpan(
-                      text: "By signing up, you agree to the ",
-                      style: TextStyle(color: Colors.black, fontSize: 14),
-                      children: [
-                        TextSpan(
-                          text: "Terms of Service",
-                          style: TextStyle(color: mainPurpleColor),
-                        ),
-                        TextSpan(
-                          text: " and ",
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        TextSpan(
-                          text: "Privacy Policy",
-                          style: TextStyle(color: mainPurpleColor),
-                        ),
-                      ],
+              ),
+              SizedBox(height: 16),
+              TextFormField(
+                onChanged: (value) {
+                  password = value;
+                },
+                obscureText: !_isPasswordVisible, // Toggle password visibility
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(color: textSecondaryColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                    borderSide: BorderSide(color: mainPurpleColor),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Checkbox(
+                    value: _isChecked,
+                    onChanged: (value) {
+                      setState(() {
+                        _isChecked = value ?? false;
+                      });
+                    },
+                    activeColor: mainPurpleColor,
+                  ),
+                  Expanded(
+                    child: RichText(
+                      text: TextSpan(
+                        text: "By signing up, you agree to the ",
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                        children: [
+                          TextSpan(
+                            text: "Terms of Service",
+                            style: TextStyle(color: mainPurpleColor),
+                          ),
+                          TextSpan(
+                            text: " and ",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                          TextSpan(
+                            text: "Privacy Policy",
+                            style: TextStyle(color: mainPurpleColor),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
+                ],
+              ),
+              if (errorMessage.isNotEmpty)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    errorMessage,
+                    style: TextStyle(color: Colors.red),
+                  ),
                 ),
-              ],
-            ),
-            if (errorMessage.isNotEmpty)
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
+              SizedBox(height: 20),
+              CustomButton(
+                text: "Sign Up",
+                backgroundColor: _isChecked ? mainPurpleColor : Colors.grey,
+                textColor: _isChecked ? backgroundBaseColor : Colors.black54,
+                onPressed: _isChecked
+                    ? () async {
+                        await _registerUser();
+                      }
+                    : () {}, // No-op function when unchecked
+              ),
+              SizedBox(height: 16),
+              Center(
                 child: Text(
-                  errorMessage,
-                  style: TextStyle(color: Colors.red),
+                  "Or with",
+                  style: TextStyle(
+                    color: textSecondaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  ),
                 ),
               ),
-            SizedBox(height: 20),
-            CustomButton(
-              text: "Sign Up",
-              backgroundColor: _isChecked ? mainPurpleColor : Colors.grey,
-              textColor: _isChecked ? backgroundBaseColor : Colors.black54,
-              onPressed: _isChecked
-                  ? () async {
-                      await _registerUser();
-                    }
-                  : () {}, // No-op function when unchecked
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: Text(
-                "Or with",
-                style: TextStyle(
-                  color: textSecondaryColor,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
+              SizedBox(height: 16),
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    text: "Already have an account? ",
+                    style: TextStyle(color: textSecondaryColor, fontSize: 16),
+                    children: [
+                      underlineTextSpan(
+                        text: "Login",
+                        onTap: () {
+                          Navigator.pushNamed(context, LogInPage.id);
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 16),
-            Center(
-              child: Text.rich(
-                TextSpan(
-                  text: "Already have an account? ",
-                  style: TextStyle(color: textSecondaryColor, fontSize: 16),
-                  children: [
-                    underlineTextSpan(
-                      text: "Login",
-                      onTap: () {
-                        Navigator.pushNamed(context, LogInPage.id);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
